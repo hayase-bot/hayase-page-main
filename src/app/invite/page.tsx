0f4570ef34571f58e1c85e5d96228e0bc78b25bb
dayrain <blackwaterbread@gmail.com>
2024-10-10 03:09:02 +0900

    feat: add /invite

---
+++ src/app/invite/page.tsx
@@ -0,0 +1,9 @@
+import { redirect } from "next/navigation";
+
+const inviteURL = 'https://discord.com/oauth2/authorize?client_id=1282545480630272010&permissions=2147485712&integration_type=0&scope=bot';
+
+function Invite() {
+  return redirect(inviteURL);
+}
+
+export default Invite;
