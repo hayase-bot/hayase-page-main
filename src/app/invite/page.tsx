51796654bfb554884b74dcbb32bcaaef7ba2e565
dayrain <blackwaterbread@gmail.com>
2024-10-11 02:49:13 +0900

    misc: invite link

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
-const inviteURL = 'https://discord.com/oauth2/authorize?client_id=1282545480630272010&permissions=2147485712&integration_type=0&scope=bot';
+const inviteURL = 'https://discord.com/oauth2/authorize?client_id=1282545480630272010&permissions=2147493904&integration_type=0&scope=bot';
 
 function Invite() {
   return redirect(inviteURL);
